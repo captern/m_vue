8c05283f22b02ce4df92d9a95393903b95c1c5eb
captern <captern@icloud.com>
2018-12-09 17:34:29 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <!--<div id="app" @touchmove.prevent>-->
   <div id="app">
     <!--keep-alive 为了将页面状态缓存，防止重新渲染-->
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
+    <!--<keep-alive>-->
+      <!--<router-view></router-view>-->
+    <!--</keep-alive>-->
+    <router-view></router-view>
     <!--<router-link to="/recommend">cdascdscsacs</router-link>-->
   </div>
 </template>
